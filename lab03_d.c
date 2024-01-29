adae56a56fd7c4b89fb95ef91a62b3daf4710f0a
ET0E <ingramrt@DESKTOP-RLGUTUR>
2024-01-29 18:56:38 -0500

    jlkjlk

--- lab03_d.c
+++ lab03_d.c
@@ -3,7 +3,8 @@
 
 int main() {
     // The list of args must end with a NULL
-    execlp("cal", "some random string", "-3", "5", "2045", NULL);
+    char* run_args[] = {"some random string", "-3", "5", "2045", NULL};
+    execvp("cal", run_args);
     perror("After exec()");
     printf("Just checking\n");
     return 0;
